e001a2e120fcc2e07221f547e0f5c502b915e07b
temasarkisov <a.sarkisov.s@yandex.ru>
2024-02-09 17:57:58 +0200

    Initial Commit

---
+++ AppWithNetworkService/AppWithNetworkServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWithNetworkServiceApp.swift
+//  AppWithNetworkService
+//
+//  Created by Артем Саркисов on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppWithNetworkServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
